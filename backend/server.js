a1f7ebebc1ea8374e5911e173e028f5bd6a47ada
Sidharth Miglani <sidhumig@gmail.com>
2020-04-17 17:06:16 -0700

    add routes to main file

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,13 @@ connection.once('open', ()=>{
     console.log("Mongo DB connection up");
 })
 
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('./exercises', exercisesRouter);
+app.use('./users', usersRouter);
+
+
 app.listen(port,()=>{
     console.log(`Server is running on ${port}`);
 })
