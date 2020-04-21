946880f02365a184d9d0925ec7d3a5313a0bc615
Sidharth Miglani <sidhumig@gmail.com>
2020-04-20 23:11:03 -0700

    navbar component

--- frontend/exercise-tracker/src/components/navbar.js
+++ frontend/exercise-tracker/src/components/navbar.js
@@ -1,4 +1,4 @@
-import React, { component, Component } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
