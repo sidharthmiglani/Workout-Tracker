fa4cce99dfe82ee2ea7be464c96329ad3aee41b4
Sidharth Miglani <sidhumig@gmail.com>
2020-04-20 22:55:32 -0700

    added components

---
+++ frontend/exercise-tracker/src/components/create-user.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+export default class CreateUser extends Component{
+    render(){
+        return(
+            <div> Create User component</div>
+        )
+    }
+}
