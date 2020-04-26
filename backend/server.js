dddbc09b1fbb398c2eed9df6de5ed04fe9bdd29e
Sidharth Miglani <sidhumig@gmail.com>
2020-04-25 21:24:20 -0700

    final

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
-
+ 
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
