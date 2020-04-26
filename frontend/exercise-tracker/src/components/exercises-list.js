410e536eb91e6d363011da99b71f1b5496d3a027
Sidharth Miglani <sidhumig@gmail.com>
2020-04-25 19:03:10 -0700

    crud request

--- frontend/exercise-tracker/src/components/exercises-list.js
+++ frontend/exercise-tracker/src/components/exercises-list.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
 
 export default class ExercisesList extends Component {
+    constructor(props) {
+        super(props);
+        this.deleteExercise = this.deleteExercise.bind(this);
+        this.state = { exercises: [] };
+
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/exercises/')
+            .then(res => {
+                this.setState({ exercises: res.data })
+            }).catch(err => console.log(err))
+    }
+
     render() {
         return (
             <div>
